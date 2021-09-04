de0aa76c215bc487416925e71e995b6590c0b7bd
Mark Kahn <m-github@vouchr.com>
2021-09-04 16:12:27 +0200

    removing promise-repeat-temp

--- src/modules/promise-repeat-temp/build/index.d.ts
+++
@@ -1,8 +0,0 @@
-export default function retryPromise<PromiseResult, Args>(fn: (...args: Args[]) => Promise<PromiseResult>, options?: {
-    maxAttempts?: number;
-    timeoutMs?: number;
-    throttleMs?: number;
-    throttleFn?: (retryCount: number, throttleMs: number) => number;
-    shouldRetryFn?: (err: unknown, retryCount: number) => boolean;
-    resolveAfterReject?: (result: PromiseResult) => void;
-}): (...args: Args[]) => Promise<PromiseResult>;
