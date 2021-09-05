2864b53ade729b281f1bea748e6e1f87b2239510
Mark Kahn <m-github@vouchr.com>
2021-09-05 13:06:04 +0200

    Adding explicit return type to module

--- src/modules/path-resolve/src/index.ts
+++ src/modules/path-resolve/src/index.ts
@@ -1,8 +1,11 @@
 import tilde from 'expand-tilde';
 import { resolve } from 'path';
+
+// eslint-disable-next-line
 __dirname = __dirname || process.cwd();
 
-export default function pathResolve(src: string) {
+export default function pathResolve(src: string): string {
   const expandedSrc = tilde(src);
+
   return expandedSrc[0] === '/' ? expandedSrc : resolve(expandedSrc);
 }
