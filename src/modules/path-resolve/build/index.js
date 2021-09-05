605472bc61eb65e8dd06c44b8793c75f37dd7cc4
Mark Kahn <m-github@vouchr.com>
2021-09-05 13:03:19 +0200

    rebuilding path-resolve to commonjs

--- src/modules/path-resolve/build/index.js
+++ src/modules/path-resolve/build/index.js
@@ -1,7 +1,11 @@
-import tilde from 'expand-tilde';
-import { resolve } from 'path';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const expand_tilde_1 = (0, tslib_1.__importDefault)(require("expand-tilde"));
+const path_1 = require("path");
 __dirname = __dirname || process.cwd();
-export default function pathResolve(src) {
-    const expandedSrc = tilde(src);
-    return expandedSrc[0] === '/' ? expandedSrc : resolve(expandedSrc);
+function pathResolve(src) {
+    const expandedSrc = (0, expand_tilde_1.default)(src);
+    return expandedSrc[0] === '/' ? expandedSrc : (0, path_1.resolve)(expandedSrc);
 }
+exports.default = pathResolve;
